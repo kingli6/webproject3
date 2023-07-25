5e4412bdd54fe91ff322f159fc6944fa340f2a66
Joanthan Fisher <jonathan.fshr@gmail.com>
2023-07-25 09:31:44 +0200

    used userManager and claims

--- College-API/Controllers/AuthController.cs
+++ College-API/Controllers/AuthController.cs
@@ -35,10 +35,19 @@ namespace College_API.Controllers
             var result = await _userManager.CreateAsync(user, model.Password!);
             if (result.Succeeded)
             {
+                // creating a claim called Admin and setting IsAdmin to true    220504_13.. 1:25:00
+                if (model.IsAdmin)
+                {
+                    await _userManager.AddClaimAsync(user, new Claim("Admin", "true"));
+                }
+                await _userManager.AddClaimAsync(user, new Claim("User", "true"));
+                await _userManager.AddClaimAsync(user, new Claim(ClaimTypes.Name, user.UserName));
+                await _userManager.AddClaimAsync(user, new Claim(ClaimTypes.Email, user.Email));
+                await _userManager.AddClaimAsync(user, new Claim(ClaimTypes.NameIdentifier, user.Id));
                 var userData = new UserViewModel
                 {
                     UserName = user.UserName,
-                    Token = CreateJwtToken(user)
+                    Token = await CreateJwtToken(user)
                 };
                 return StatusCode(201, userData);
             }
@@ -46,35 +55,54 @@ namespace College_API.Controllers
             {
                 foreach (var error in result.Errors)
                 {
-                    ModelState.AddModelError("User registration", error.Description);
+                    ModelState.AddModelError("User registration error. ", error.Description);
                 }
                 return StatusCode(500, ModelState);
             }
         }
-
+        //user@example.com, Password!1
+        //"userName": "joe@gmail.com", "password": "Password!1"
         [HttpPost("login")]
-        public ActionResult Login(LoginViewModel model)
+        public async Task<ActionResult<UserViewModel>> Login(LoginViewModel model)
         {
-            if (model.UserName == "Joe" && model.Password == "password!")
+            var user = await _userManager.FindByNameAsync(model.UserName!);
+            if (user is null)
+                return Unauthorized("Wrong username or password???");
+
+            var result = await _signInManager.CheckPasswordSignInAsync(user, model.Password!, false);   //false is for lockoutonfailure .false meaning; we won't lock them out
+
+            if (!result.Succeeded)
+                return Unauthorized();
+
+            var userData = new UserViewModel
             {
-                return Ok(new
-                {
-                    access_token = "" //CreateJwtToken(model.UserName)
-                });
-            }
-            return Unauthorized();
+                UserName = user.UserName,
+                Token = await CreateJwtToken(user)
+            };
+
+            return Ok(userData);
+            // if (model.UserName == "Joe" && model.Password == "password!")
+            // {
+            //     return Ok(new
+            //     {
+            //         access_token = CreateJwtToken(model.UserName)
+            //     });
+            // }
+            // return Unauthorized();
         }
         //[Authorize("Administrator")]
-        private string CreateJwtToken(IdentityUser user)
+        private async Task<string> CreateJwtToken(IdentityUser user)
         {// jwt.io  to control the token
             var key = Encoding.ASCII.GetBytes(_config.GetValue<string>("apiKey")!);
-            var claims = new List<Claim>{
-                new Claim(ClaimTypes.Name, user.UserName!),
-                new Claim(ClaimTypes.Email, user.Email!),
-                // new Claim("Admin", "true"),
-            };
+            // var claims = new List<Claim>{
+            //     new Claim(ClaimTypes.Name, user.UserName!),
+            //     new Claim(ClaimTypes.Email, user.Email!),
+            //     // new Claim("Admin", "true"),
+            // };
+
+            var userClaims = await _userManager.GetClaimsAsync(user);//220504_13..1:35:00
             var jwt = new JwtSecurityToken(
-                claims: claims,
+                claims: userClaims,//claims,
                 notBefore: DateTime.Now,
                 expires: DateTime.Now.AddDays(1),
                 signingCredentials: new SigningCredentials(
--- College-API/ViewModels/RegisterUserViewModel/RegisterUserViewModel.cs
+++ College-API/ViewModels/RegisterUserViewModel/RegisterUserViewModel.cs
@@ -1,8 +1,4 @@
-using System;
-using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace College_API.ViewModels.RegisterUserViewModel
 {
@@ -13,5 +9,6 @@ namespace College_API.ViewModels.RegisterUserViewModel
         public string? Email { get; set; }
         [Required]
         public string? Password { get; set; }
+        public bool IsAdmin { get; set; } = false;
     }
 }
