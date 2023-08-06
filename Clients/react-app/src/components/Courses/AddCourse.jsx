8e36f13800fc63448ee5cc67b3604068c70ac37e
Joanthan Fisher <jonathan.fshr@gmail.com>
2023-08-06 22:18:09 +0200

    form Add-course

--- Clients/react-app/src/components/Courses/AddCourse.jsx
+++ Clients/react-app/src/components/Courses/AddCourse.jsx
@@ -1,34 +1,51 @@
+import { useState } from 'react';
+
 function AddCourse() {
+  const [courseNum, setCourseNum] = useState('');
+
+  const onHandlerCourseNumTextChange = (e) => {
+    console.log('texten är ändrad');
+    console.log(e);
+    setCourseNum();
+  };
   return (
     <>
       <h1 clasNames="page-title">Add a new course</h1>
       <section className="form-container">
         <h4>Course somethingsomething</h4>
-        <form className="form" asp-action="Create" method="post">
-          <div className="form-control">
-            <label asp-for="@Model!.CourseNumber"></label>
-            <input asp-for="@Model!.CourseNumber" />
-          </div>
-          <div className="form-control">
-            <label asp-for="@Model!.Name"></label>
-            <input asp-for="@Model!.Name" />
-          </div>
-          <div className="form-control">
-            <label asp-for="@Model!.Duration"></label>
-            <input asp-for="@Model!.Duration" />
-          </div>
-          <div className="form-control">
-            <label asp-for="@Model!.Description"></label>
-            <input asp-for="@Model!.Description" />
-          </div>
-          <div className="form-control">
-            <label asp-for="@Model!.Details"></label>
-            <textarea asp-for="@Model!.Details"></textarea>
-          </div>
-          <button type="submit" className="btn">
-            Save
-          </button>
-        </form>
+        <section className="form-wrapper">
+          <form className="form" method="post">
+            <div className="form-control">
+              <label htmlFor="">Course Number</label>
+              <input
+                onChange={onHandlerCourseNumTextChange}
+                value="courseNumber"
+                type="text"
+                id="courseNumber"
+                name="courseNumber"
+              />
+            </div>
+            <div className="form-control">
+              <label htmlFor="name">Name</label>
+              <input type="text" id="name" name="name" />
+            </div>
+            <div className="form-control">
+              <label htmlFor="duration">Duration</label>
+              <input type="text" id="duration" name="duration" />
+            </div>
+            <div className="form-control">
+              <label htmlFor="description">Description</label>
+              <input type="text" id="description" name="description" />
+            </div>
+            <div className="form-control">
+              <label htmlFor="details">Course Details</label>
+              <textarea type="text" id="details" name="details"></textarea>
+            </div>
+            <button type="submit" className="btn">
+              Save
+            </button>
+          </form>
+        </section>
       </section>
     </>
   );
